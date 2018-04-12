1a0f7876895765b8982ea3e95474084447bb738a
zherrin85 <zherrin85@gmail.com>
2018-04-11 19:07:04 -0700

    Initial commit

---
+++ duck.sh
@@ -0,0 +1,2 @@
+echo url="https://www.duckdns.org/update?domains=zachub&token=f7664edf-d10f-4dc5-8850-213901c77141&ip=" | curl -k -o /opt/Scripts/duckdns/zachub.log -K - &&
+echo url="https://www.duckdns.org/update?domains=herrinhass&token=f7664edf-d10f-4dc5-8850-213901c77141&ip=" | curl -k -o /opt/Scripts/duckdns/herrinhass.log -K -
